d5943e8609d03c7b090449773e5f3f4431343bd2
Pasaa2025 <kerolosmonir27@gmail.com>
2025-03-07 16:28:18 +0200

    so==controller_policy_nologic

---
+++ Amanek/Areas/Admin/Controllers/PolicyController.cs
@@ -0,0 +1,23 @@
+﻿using DataAccess.Repository;
+using DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Utility;
+
+namespace Amanek.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = $"{SD.AdminRole}, {SD.CompanyRole}")]
+    public class PolicyController : Controller
+    {
+        private readonly IUnitOfWorks unitOfWorks;
+
+        public PolicyController(IUnitOfWorks unitOfWorks) {
+            this.unitOfWorks = unitOfWorks;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
